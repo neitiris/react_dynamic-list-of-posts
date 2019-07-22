662162c966c3b414b4a0fc61ca129e3b465c67ec
neitiris <Veliantos@gmail.com>
2019-07-22 22:09:54 +0300

    Updates

---
+++ precache-manifest.bc1278c04006011595b976e7141457bb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bec85c8860a2dabea2b864406f71373",
+    "url": "/react_dynamic-list-of-posts/index.html"
+  },
+  {
+    "revision": "12733e229a8f775284d8",
+    "url": "/react_dynamic-list-of-posts/static/css/main.04863015.chunk.css"
+  },
+  {
+    "revision": "35cf28954eff939c29f9",
+    "url": "/react_dynamic-list-of-posts/static/js/2.1cff5df2.chunk.js"
+  },
+  {
+    "revision": "12733e229a8f775284d8",
+    "url": "/react_dynamic-list-of-posts/static/js/main.b732920e.chunk.js"
+  },
+  {
+    "revision": "fc988510490290486853",
+    "url": "/react_dynamic-list-of-posts/static/js/runtime~main.69709ad2.js"
+  }
+]);
